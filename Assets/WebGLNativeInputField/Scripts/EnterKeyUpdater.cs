a41d464224609a71bd05f25b6624b2dfa89efeba
Yu Tanaka <YRK00337@nifty.com>
2020-06-18 22:41:14 +0900

    修正実験

---
+++ Assets/WebGLNativeInputField/Scripts/EnterKeyUpdater.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnterKeyUpdater : MonoBehaviour
+{
+    WebGLNativeInputField webGLIF = null;
+    [SerializeField]
+    Text outputText = null;
+
+    private void Awake()
+    {
+        webGLIF = GetComponent<WebGLNativeInputField>();
+    }
+
+    public void Changed()
+    {
+        Debug.Log($"Changed {webGLIF.text} {webGLIF.textComponent.text}");
+        outputText.text = webGLIF.text;
+    }
+
+    public void Ended()
+    {
+        Debug.Log("Ended {webGLIF.text}");
+    }
+
+}
